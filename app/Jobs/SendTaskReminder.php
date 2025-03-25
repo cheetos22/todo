c39571e817fdb280370ddbb844e7f915c5c2ecc4
cheetos22 <cheetos22@gmail.com>
2025-03-25 16:53:59 +0100

    add job

---
+++ app/Jobs/SendTaskReminder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\TaskReminderMail;
+use App\Models\Task;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendTaskReminder implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $task;
+
+    public function __construct(Task $task)
+    {
+        $this->task = $task;
+    }
+
+    public function handle()
+    {
+        Mail::to($this->task->user->email)->send(new TaskReminderMail($this->task));
+    }
+}
