c96940aeea131bee6e640640ea3bd78eb2ab45c0
cheetos22 <cheetos22@gmail.com>
2025-03-25 16:57:04 +0100

    add routes form tasks

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/tasks/public/{token}', [TaskController::class, 'showPublic'])->name('tasks.public');
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -19,7 +22,10 @@ Route::middleware('auth')->group(function () {
 
     Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
     Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+    Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+    Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
     Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+    Route::post('/tasks/{task}/share', [TaskController::class, 'generatePublicLink'])->name('tasks.share');
 });
 
 require __DIR__.'/auth.php';
