3dc016042cd3555ce4d7834114ecb6315f1b1f8a
cheetos22 <cheetos22@gmail.com>
2025-03-25 16:53:41 +0100

    add validate request

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,6 +7,7 @@ use App\Models\Task;
 use App\Services\TaskService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class TaskController extends Controller
 {
@@ -23,6 +24,19 @@ class TaskController extends Controller
         return view('tasks.index', compact('tasks'));
     }
 
+    public function edit(Task $task)
+    {
+
+        return view('tasks.edit', compact('task'));
+    }
+
+    public function update(TaskRequest $request, Task $task)
+    {
+
+        $this->taskService->updateTask($task, $request->validated());
+        return redirect()->route('tasks.index');
+    }
+
     public function store(TaskRequest $request)
     {
         $this->taskService->createTask($request->validated());
@@ -36,4 +50,30 @@ class TaskController extends Controller
         }
         return redirect()->route('tasks.index');
     }
+
+    public function generatePublicLink(Task $task)
+    {
+
+        if ($task->public_token && $task->public_token_expires_at > now()) {
+            return back()->with('public_link', route('tasks.public', ['token' => $task->public_token]));
+        }
+
+        $task->public_token = Str::random(32);
+        $task->public_token_expires_at = now()->addSeconds(60);
+        $task->save();
+
+        return back()->with('public_link', route('tasks.public', ['token' => $task->public_token]));
+    }
+
+
+    public function showPublic($token)
+{
+    $task = Task::where('public_token', $token)->first();
+
+    if (!$task || $task->public_token_expires_at < now()) {
+        abort(403, 'Link wygasł.');
+    }
+
+    return view('tasks.public', compact('task'));
+}
 }
---
+++ app/Http/Requests/TaskRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TaskRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|max:2',
+            'description' => 'nullable',
+            'priority' => 'required|in:low,medium,high',
+            'status' => 'required|in:to-do,in-progress,done',
+            'due_date' => 'required|date',
+        ];
+    }
+}
